e8cafe246d738bf49397888d6826132201f32771
M Newman <megan.m.boggs@gmail.com>
2022-10-26 20:58:47 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// document.getElementById("count").innerText = 5
+
+//initialize count as 0
+// listen for clicks on button
+//increment the count when button clicked
+//change count-el in html to show new clicks
+
+let countEl = document.getElementById("count-el");
+
+let count = 0;
+
+function increment() {
+  count = count + 1;
+  countEl.innerText = count;
+}
